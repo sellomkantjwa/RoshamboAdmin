7bcc85c8d929fa21b46190d4e647e0ef9ae131af
Sello182 <sello.mkantjwa@entelect.co.za>
2015-02-11 13:20:54 +0200

    created new project

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RoshamboAdministrator
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoshamboAdministrator.MvcApplication" Language="C#" %>
